fdeed4e1f9a4438c5ebec5e6b2115ed184c2dfa4
bgoggin <brgoggs@gmail.com>
2017-07-29 17:44:52 -0700

    Creating branch using Carto PostGIS databases

--- public/javascripts/example.js
+++ public/javascripts/example.js
@@ -5,16 +5,75 @@ var basemapUrl = 'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png';
 var basemapAttribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>';
 var basemapProperties = {minZoom: 2, maxZoom: 16, attribution: basemapAttribution};
 L.tileLayer(basemapUrl, basemapProperties).addTo(map);
+  
+
+// Add JSON to map
+/*
+// Set CartoDB Username
+var cartoDBUserName = "bgoggin";
+
+// Get all development projects from dataset
+var sqlQuery = "SELECT * FROM current_data";
+
+
+$.getJSON("https://"+cartoDBUserName+".cartodb.com/api/v2/sql?format=GeoJSON&q="+sqlQuery, function(data) {
+    Data = L.geoJson(data, {pointToLayer: pointToLayer}).addTo(map);
+});
 
 
 //specify what the circle markers should look like (radius and fill color are dynamically set later)
-var markerStyle = {radius: null, fillOpacity: 0.7, color: '#666666', opacity: 1, weight: 1, fillColor: null};
+//var markerStyle = {radius: null, fillOpacity: 0.7, color: '#666666', opacity: 1, weight: 1, fillColor: null};
+  
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+	var initialZoomLevel = 12;
+} else {
+	var initialZoomLevel = 10;
+}
+  
+function getRadius() {
+	//Make dots bigger if viewed on mobile
+	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+	    if (map.getZoom()) { radius = Math.pow(map.getZoom(), 0.9); } //if map already exists, get current zoom level
+	    else { radius = Math.pow(initialZoomLevel, 0.9); } //if it's the initial startup, use initial zoom level
+	    return radius;
+	} else {
+	    if (map.getZoom()) { radius = Math.pow(map.getZoom(), 0.8); } //if map already exists, get current zoom level
+	    else { radius = Math.pow(initialZoomLevel, 0.8); } //if it's the initial startup, use initial zoom level
+	    return radius;
+	}	
+}
+  
+  
+// function to add data points to map layer with proper styling
+function pointToLayer(feature, latlng) {
+        var markerStyle = {radius: null, fillOpacity: 0.7, color: '#666666', opacity: 1, weight: 1, fillColor: null};
+		markerStyle.fillColor = '#0E6698';
+		markerStyle.radius = getRadius();
+        return L.circleMarker(latlng, markerStyle);
+}*/
 
+// Add JSON to map
+L.geoJson(myData, {pointToLayer: pointToLayer}).addTo(map);
+
+//console.log(myData.features[0].geometry.coordinates);
+/*
+function filter_map() {
+  	if (map.hasLayer(myData)) {
+  		map.removeLayer(myData);
+  	}
+    //Get project status from user selections
+    var proj_status = document.getElementById("user_input").name;
+    alert('proj_status');
+}
+*/
+
+//specify what the circle markers should look like
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
     var initialZoomLevel = 12;
 } else {
     var initialZoomLevel = 10;
 }
+
 function getRadius() {
     //Make dots bigger if viewed on mobile
     if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
@@ -26,18 +85,17 @@ function getRadius() {
         else { radius = Math.pow(initialZoomLevel, 0.8); } //if it's the initial startup, use initial zoom level
         return radius;
         }
-}
+};
 
-// function to add data points to map layer with proper styling
 function pointToLayer(feature, latlng) {
-        markerStyle.fillColor = '#666666';
+        var markerStyle = {radius: null, fillOpacity: 0.7, color: '#666666', opacity: 1, weight: 1, fillColor: '#ff7800'};
+        markerStyle.fillColor = '#0E6698';
         markerStyle.radius = getRadius();
         return L.circleMarker(latlng, markerStyle);
 }
 
-// Add JSON to map
-L.geoJson(myData,{pointToLayer: pointToLayer}).addTo(map);
 
+/*
 //Add code to download PDF Report
 document.getElementById('pdf_download').addEventListener('click', function() {
     leafletImage(map, downloadMap);
@@ -156,4 +214,4 @@ function downloadMap(err, canvas) {
 	});
 
     pdf.save("download.pdf");
-};
+};*/
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express'); // require Express
 var router = express.Router(); // setup usage of the Express router engine
 var json2csv = require('json2csv');
+var jquery = require('jQuery');
+var CartoDB = require('cartodb');
 
+//I comment the below section out because I am opting to use Carto PostGIS for now instead.
 
-/* PostgreSQL and PostGIS module and connection setup */
+// PostgreSQL and PostGIS module and connection setup 
 var pg = require("pg"); // require Postgres module
 
 // Setup connection
@@ -16,14 +19,16 @@ var conString = "postgres://"+username+":"+password+"@"+host+"/"+database; // Yo
 var variables = "nameaddr, net_units, net_aff_units, net_ret, net_mips, net_cie, net_pdr, net_med, net_visit, beststat"
 
 // Set up your database query to display GeoJSON
-var dev_query = "SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(lg.geom)::json As geometry, row_to_json((" + variables + ")) As properties FROM dev_pipeline As lg) As f) As fc";
+//var dev_query = "SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(lg.geom)::json As geometry, row_to_json((" + variables + ")) As properties FROM dev_pipeline As lg) As f) As fc";
+
+var dev_query = "SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(lg.geom)::json As geometry, row_to_json((nameaddr, net_units, net_aff_units, net_ret, net_mips, net_cie, net_pdr, net_med, net_visit, beststat)) As properties FROM dev_pipeline As lg, (SELECT * FROM neighborhoods_41 WHERE nhood = 'North Beach') as dd_nc WHERE ST_Intersects(lg.geom, dd_nc.geom)) As f) As fc";
 
 //initialize data here so that we make it global in scope for this file
 var data = null;
 
-/* GET the map page */
+// GET the map page 
 router.get('/map', function(req, res) {
-    
+    /*
     var client = new pg.Client(conString); // Setup our Postgres Client
     client.connect(); // connect to the client
     var query = client.query(dev_query); // Run our Query
@@ -31,6 +36,7 @@ router.get('/map', function(req, res) {
         result.addRow(row);
     });
     // Pass the result to the map page
+    
     query.on("end", function (result) {
         data = result.rows[0].row_to_json // Save the JSON as variable data
         res.render('map', {
@@ -38,10 +44,32 @@ router.get('/map', function(req, res) {
             jsonData: data, // Pass data to the View
             name: dev_query
         });
+        //console.log(data.features[0].properties.f1);
+    });*/
+
+    /*
+    var myData = $.getJSON("https://"+cartoDBUserName+".cartodb.com/api/v2/sql?format=GeoJSON&q="+sqlQuery);
+    
+    //$(document).ready(function() {alert('hello');});
+    res.render('map', {
+        jsonData: data
     });
+    var message = JSON.parse("https://"+cartoDBUserName+".cartodb.com/api/v2/sql?format=GeoJSON&q="+sqlQuery);
+    */
+    var sql = new CartoDB.SQL({user:'bgoggin'})
+    sql.execute("SELECT * FROM current_data", {format: 'geojson'}).done(function(data) {
+      var carto_response = JSON.parse(data);
+      res.render('map', {
+          jsonData: carto_response
+      }); 
+      
+      //res.send(response.features[0].properties.apn);
+    });
+    
 });
 
-/* GET the filtered page */
+// GET the filtered page—commented out for now because I am using Carto API
+/*
 router.get('/filter*', function (req, res) {
     
     var name = req.query.name;
@@ -69,7 +97,7 @@ router.get('/filter*', function (req, res) {
     };
     
 });
-
+*/
 
 router.get('/csv_export', function(req, res, next) {
     var filter_query = req.query.lastname;
@@ -101,27 +129,5 @@ router.get('/csv_export', function(req, res, next) {
     });
 
 });
-
-router.post('/csv_export', function(req, res, next) {
-    
-    var data = JSON.parse(req.body.ID);
-    
-    var myArray=[];
-
-    for (i = 0; i < data.features.length; i++) {
-        var myObject = {'nameaddr': data.features[i].properties.f1, 'net_units': data.features[i].properties.f2, 'beststat': data.features[i].properties.f10};
-        myArray.push(myObject);
-    }
-    //res.send(myArray[0].nameaddr);
-    
-    var fields = ['nameaddr', 'net_units', 'beststat'];
-    var csv = json2csv({data: myArray, fields: fields});
-    res.setHeader('Content-disposition', 'attachment; filename=data.csv');
-    res.set('Content-Type', 'text/csv');
-    res.status(200).send(csv);
-
-});
-
-
 
 module.exports = router;
