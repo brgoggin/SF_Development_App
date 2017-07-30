9a09aced97ca2c5304f12404766b8389b7687691
bgoggin <brgoggs@gmail.com>
2017-07-30 00:23:48 -0700

    Got basic functionality working

--- public/javascripts/example.js
+++ public/javascripts/example.js
@@ -8,64 +8,7 @@ L.tileLayer(basemapUrl, basemapProperties).addTo(map);
   
 
 // Add JSON to map
-/*
-// Set CartoDB Username
-var cartoDBUserName = "bgoggin";
-
-// Get all development projects from dataset
-var sqlQuery = "SELECT * FROM current_data";
-
-
-$.getJSON("https://"+cartoDBUserName+".cartodb.com/api/v2/sql?format=GeoJSON&q="+sqlQuery, function(data) {
-    Data = L.geoJson(data, {pointToLayer: pointToLayer}).addTo(map);
-});
-
-
-//specify what the circle markers should look like (radius and fill color are dynamically set later)
-//var markerStyle = {radius: null, fillOpacity: 0.7, color: '#666666', opacity: 1, weight: 1, fillColor: null};
-  
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-	var initialZoomLevel = 12;
-} else {
-	var initialZoomLevel = 10;
-}
-  
-function getRadius() {
-	//Make dots bigger if viewed on mobile
-	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-	    if (map.getZoom()) { radius = Math.pow(map.getZoom(), 0.9); } //if map already exists, get current zoom level
-	    else { radius = Math.pow(initialZoomLevel, 0.9); } //if it's the initial startup, use initial zoom level
-	    return radius;
-	} else {
-	    if (map.getZoom()) { radius = Math.pow(map.getZoom(), 0.8); } //if map already exists, get current zoom level
-	    else { radius = Math.pow(initialZoomLevel, 0.8); } //if it's the initial startup, use initial zoom level
-	    return radius;
-	}	
-}
-  
-  
-// function to add data points to map layer with proper styling
-function pointToLayer(feature, latlng) {
-        var markerStyle = {radius: null, fillOpacity: 0.7, color: '#666666', opacity: 1, weight: 1, fillColor: null};
-		markerStyle.fillColor = '#0E6698';
-		markerStyle.radius = getRadius();
-        return L.circleMarker(latlng, markerStyle);
-}*/
-
-// Add JSON to map
 L.geoJson(myData, {pointToLayer: pointToLayer}).addTo(map);
-
-//console.log(myData.features[0].geometry.coordinates);
-/*
-function filter_map() {
-  	if (map.hasLayer(myData)) {
-  		map.removeLayer(myData);
-  	}
-    //Get project status from user selections
-    var proj_status = document.getElementById("user_input").name;
-    alert('proj_status');
-}
-*/
 
 //specify what the circle markers should look like
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
@@ -88,14 +31,13 @@ function getRadius() {
 };
 
 function pointToLayer(feature, latlng) {
-        var markerStyle = {radius: null, fillOpacity: 0.7, color: '#666666', opacity: 1, weight: 1, fillColor: '#ff7800'};
-        markerStyle.fillColor = '#0E6698';
+        var markerStyle = {radius: null, fillOpacity: 0.7, color: '#666666', opacity: 1, weight: 1, fillColor: '#0E6698'};
         markerStyle.radius = getRadius();
         return L.circleMarker(latlng, markerStyle);
 }
 
 
-/*
+
 //Add code to download PDF Report
 document.getElementById('pdf_download').addEventListener('click', function() {
     leafletImage(map, downloadMap);
@@ -103,13 +45,12 @@ document.getElementById('pdf_download').addEventListener('click', function() {
 
 
 function downloadMap(err, canvas) {
+
     var imgData = canvas.toDataURL();
     var dimensions = map.getSize();
     
     var pdf = new jsPDF('p', 'pt', 'letter');
     pdf.addImage(imgData, 'PNG', 10, 10, dimensions.x * 0.5, dimensions.y * 0.5);
-    
-    //pdf.rect(130, 350, 60, 60); test rectangle
 
 	var columns = [
 	{title: "Address", dataKey: "address"},
@@ -165,9 +106,9 @@ function downloadMap(err, canvas) {
 
     for (i = 0; i < 30; i++) {
         var props = myData.features[i].properties;
-        if (props.f10 == 'CONSTRUCTION') {
-            var obj = {"address": props.f1.toLocaleString(), "net_units": props.f2.toLocaleString(), "net_aff_units": props.f3.toLocaleString(), "net_ret": props.f4.toLocaleString(),
-            "net_mips": props.f5.toLocaleString(), "net_cie": props.f6.toLocaleString(), "net_pdr": props.f7.toLocaleString(), "net_med": props.f8.toLocaleString(), "net_visit": props.f9.toLocaleString()};
+        if (props.status == 'CONSTRUCTION') {
+            var obj = {"address": props.address.toLocaleString(), "net_units": props.net_units.toLocaleString(), "net_aff_units": props.net_aff_units.toLocaleString(), "net_ret": props.net_ret.toLocaleString(),
+            "net_mips": props.net_mips.toLocaleString(), "net_cie": props.net_cie.toLocaleString(), "net_pdr": props.net_pdr.toLocaleString(), "net_med": props.net_med.toLocaleString(), "net_visit": props.net_visit.toLocaleString()};
             cons_rows.push(obj);
         }
     }
@@ -180,9 +121,9 @@ function downloadMap(err, canvas) {
 
     for (i = 0; i < 30; i++) {
         var props = myData.features[i].properties;
-        if (props.f10 == 'BP ISSUED') {
-            var obj = {"address": props.f1.toLocaleString(), "net_units": props.f2.toLocaleString(), "net_aff_units": props.f3.toLocaleString(), "net_ret": props.f4.toLocaleString(),
-            "net_mips": props.f5.toLocaleString(), "net_cie": props.f6.toLocaleString(), "net_pdr": props.f7.toLocaleString(), "net_med": props.f8.toLocaleString(), "net_visit": props.f9.toLocaleString()};
+        if (props.status == 'BP ISSUED') {
+            var obj = {"address": props.address.toLocaleString(), "net_units": props.net_units.toLocaleString(), "net_aff_units": props.net_aff_units.toLocaleString(), "net_ret": props.net_ret.toLocaleString(),
+            "net_mips": props.net_mips.toLocaleString(), "net_cie": props.net_cie.toLocaleString(), "net_pdr": props.net_pdr.toLocaleString(), "net_med": props.net_med.toLocaleString(), "net_visit": props.net_visit.toLocaleString()};
             BP_rows.push(obj);
         }
     }
@@ -202,10 +143,6 @@ function downloadMap(err, canvas) {
         pdf.setFillColor(102, 178, 255);
       }
     },
-    drawRow: function (row, data) {
-        //pdf.rect(data.settings.margin.left, row.y, data.table.width, 20, 'S');
-        pdf.rect(130, data.table.height, 60, 60);
-    },
     startY: 380,
     showHeader: 'firstPage',
     styles: {overflow: 'linebreak', tableWidth: 'auto', },
@@ -214,4 +151,5 @@ function downloadMap(err, canvas) {
 	});
 
     pdf.save("download.pdf");
-};*/
+    
+};
--- routes/index.js
+++ routes/index.js
@@ -16,117 +16,65 @@ var host = "localhost"
 var database = "briangoggin" // database name
 var conString = "postgres://"+username+":"+password+"@"+host+"/"+database; // Your Database Connection
 
-var variables = "nameaddr, net_units, net_aff_units, net_ret, net_mips, net_cie, net_pdr, net_med, net_visit, beststat"
-
-// Set up your database query to display GeoJSON
-//var dev_query = "SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(lg.geom)::json As geometry, row_to_json((" + variables + ")) As properties FROM dev_pipeline As lg) As f) As fc";
-
-var dev_query = "SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(lg.geom)::json As geometry, row_to_json((nameaddr, net_units, net_aff_units, net_ret, net_mips, net_cie, net_pdr, net_med, net_visit, beststat)) As properties FROM dev_pipeline As lg, (SELECT * FROM neighborhoods_41 WHERE nhood = 'North Beach') as dd_nc WHERE ST_Intersects(lg.geom, dd_nc.geom)) As f) As fc";
-
 //initialize data here so that we make it global in scope for this file
 var data = null;
 
 // GET the map page 
 router.get('/map', function(req, res) {
-    /*
-    var client = new pg.Client(conString); // Setup our Postgres Client
-    client.connect(); // connect to the client
-    var query = client.query(dev_query); // Run our Query
-    query.on("row", function (row, result) {
-        result.addRow(row);
-    });
-    // Pass the result to the map page
-    
-    query.on("end", function (result) {
-        data = result.rows[0].row_to_json // Save the JSON as variable data
-        res.render('map', {
-            title: "Express API", // Give a title to our page
-            jsonData: data, // Pass data to the View
-            name: dev_query
-        });
-        //console.log(data.features[0].properties.f1);
-    });*/
-
-    /*
-    var myData = $.getJSON("https://"+cartoDBUserName+".cartodb.com/api/v2/sql?format=GeoJSON&q="+sqlQuery);
-    
-    //$(document).ready(function() {alert('hello');});
-    res.render('map', {
-        jsonData: data
-    });
-    var message = JSON.parse("https://"+cartoDBUserName+".cartodb.com/api/v2/sql?format=GeoJSON&q="+sqlQuery);
-    */
     var sql = new CartoDB.SQL({user:'bgoggin'})
-    sql.execute("SELECT * FROM current_data", {format: 'geojson'}).done(function(data) {
+    var query = "SELECT * FROM current_data";
+    sql.execute(query, {format: 'geojson'}).done(function(data) {
       var carto_response = JSON.parse(data);
       res.render('map', {
-          jsonData: carto_response
+          jsonData: carto_response,
+          sent_string: query
       }); 
-      
-      //res.send(response.features[0].properties.apn);
     });
     
 });
 
 // GET the filtered page—commented out for now because I am using Carto API
-/*
+
 router.get('/filter*', function (req, res) {
-    
-    var name = req.query.name;
-    if (name.indexOf("--") > -1 || name.indexOf("'") > -1 || name.indexOf(";") > -1 || name.indexOf("/*") > -1 || name.indexOf("xp_") > -1){
-        console.log("Bad request detected");
-        res.redirect('/map');
-        return;
+    var proj_status = req.query.name;
+    if (proj_status == 'All') {
+        var statusvar = "";
     } else {
-        console.log("Request passed")
-        var filter_query = "SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(lg.geom)::json As geometry, row_to_json((" + variables + ")) As properties FROM dev_pipeline As lg WHERE lg.beststat = \'" + name + "\') As f) As fc";
-        var client = new pg.Client(conString);
-        client.connect();
-        var query = client.query(filter_query);
-        query.on("row", function (row, result) {
-            result.addRow(row);
-        });
-        query.on("end", function (result) {
-            data = result.rows[0].row_to_json
-            res.render('map', {
-                title: "Express API",
-                jsonData: data,
-                name: filter_query
-            });
-        });
-    };
+        var statusvar = " WHERE status = '" + proj_status + "'";
+    }
+    var query = "SELECT * FROM current_data" + statusvar;
+
+    var sql = new CartoDB.SQL({user:'bgoggin'})
+    sql.execute(query, {format: 'geojson'}).done(function(data) {
+      var carto_response = JSON.parse(data);
+      res.render('map', {
+          jsonData: carto_response,
+          sent_string: query
+      });
+    });
     
 });
-*/
+
 
 router.get('/csv_export', function(req, res, next) {
-    var filter_query = req.query.lastname;
+    var query = req.query.lastname;
     
-    var data = null;
-    var client = new pg.Client(conString);
-    client.connect();
-    var query = client.query(filter_query);
-    query.on("row", function (row, result) {
-        result.addRow(row);
-    });
-    query.on("end", function (result) {
-        data = result.rows[0].row_to_json
-        //res.send(data.features[0].properties.f1);
-        //res.send(JSON.stringify(data));
-        
-        var myArray=[];
+    var sql = new CartoDB.SQL({user:'bgoggin'})
+    
+    sql.execute(query, {format: 'geojson'}).done(function(data) {
+      var carto = JSON.parse(data);
+      var myArray=[];
 
-        for (i = 0; i < data.features.length; i++) {
-            var myObject = {'nameaddr': data.features[i].properties.f1, 'net_units': data.features[i].properties.f2, 'beststat': data.features[i].properties.f10};
-            myArray.push(myObject);
-        }
-        //res.send(myArray[0].nameaddr);
-        var fields = ['nameaddr', 'net_units', 'beststat'];
-        var csv = json2csv({data: myArray, fields: fields});
-        res.setHeader('Content-disposition', 'attachment; filename=data.csv');
-        res.set('Content-Type', 'text/csv');
-        res.status(200).send(csv);
-    });
+      for (i = 0; i < carto.features.length; i++) {
+          var myObject = {'Address': carto.features[i].properties.address, 'Net Units': carto.features[i].properties.net_units, 'Status': carto.features[i].properties.status};
+          myArray.push(myObject);
+      }
+      var fields = ['Address', 'Net Units', 'Status'];
+      var csv = json2csv({data: myArray, fields: fields});
+      res.setHeader('Content-disposition', 'attachment; filename=data.csv');
+      res.set('Content-Type', 'text/csv');
+      res.status(200).send(csv);
+      });
 
 });
 
